488d754ab6777b5c41573b7077e149528f129635
Yandiev <azizdavronov2005@gmail.com>
2025-08-24 14:23:44 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,114 @@
-import { useRef } from "react";
-import setStorge from "./storge";
+import { useEffect, useState } from "react";
 
-function App() {
-  const { second, setSecond } = setStorge();
-  const intervalIdRef = useRef(null);
+export default function App() {
+  const [todo,setTodo] = useState( () => {
+    const saved = localStorage.getItem("users");
+    return saved ? JSON.parse(saved) : [];
+  })
+  const [isCheked,setCheked] = useState("all")
+  const [inputValue,setInputValue] = useState("")
+  function todoSet() {
+    if (!inputValue.trim()) return;
 
-  function setTime() {
-    if (intervalIdRef.current) return;
-    const time = setInterval(() => {
-      setSecond(prev => prev + 1); 
-    }, 100);
+    const date = new Date();
+    const formattedDate = date.toLocaleString();
 
-    intervalIdRef.current = time;
+    const newTodo = {
+      id: todo.length + 1,
+      name: inputValue,
+      data: formattedDate,
+      status: true
+    };
+    
+    setTodo([...todo, newTodo]);
+    setInputValue("");
   }
-
-  function killTime() {
-    clearInterval(intervalIdRef.current);
-    intervalIdRef.current = null;
+  useEffect(()=>{
+    localStorage.setItem("users",JSON.stringify(todo));
+  },[todo])
+  function ischek(btn) {
+    document.querySelectorAll("button").forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+    console.log(btn.className)
+    setCheked(btn.value);
   }
+  function toggleStatus(id) {
+    let updatedTodos;
 
+    if (id){
+      updatedTodos = todo.map(item =>
+        item.id === id ? { ...item, status: !item.status } : item
+      );
+    }
+    else{
+      updatedTodos = todo.map(item => ({
+      ...item,
+      status: true
+    }));
+    }
+    setTodo(updatedTodos);
+  }
   return (
-    <div className="max-w-[200px] mx-auto border my-20 flex flex-col gap-5 p-10">
-      <span className="p-2 border text-center w-16 mx-auto">{second}</span>
-      <div className="flex justify-between">
-        <button
-          className="border p-2 rounded-xl active:bg-red-500"
-          onClick={setTime}
-        >
-          Start
+    <div className="max-w-3xl mx-auto font-light">
+      <nav className="flex  justify-between my-10 items-center  font-bold">
+        <div className="text-2xl">Todo List</div>
+        <button className="boder shadow-lg p-4 rounded-2xl active:opacity-75" onClick={()=>toggleStatus()}>
+          Barchasini bajarilgan qilish
         </button>
-        <button
-          className="border p-2 rounded-xl active:bg-blue-500"
-          onClick={killTime}
-        >
-          Stop
-        </button>
+      </nav>
+      <main className="flex justify-between gap-5 italic">
+        <div className="flex-1/3  p-2  rounded-2xl flex justify-between items-center shadow-lg">
+          <input type="text" placeholder="Yangi vazifa... (Enter)"value={inputValue}  onChange={el => setInputValue(el.target.value)} 
+            onKeyDown={e => {
+            if (e.key === "Enter") {
+              todoSet();
+            }
+          }} />
+          <button className="boder shadow-lg  p-4 rounded-2xl active:opacity-75 bg-[#2c2a2a] text-white" onClick={todoSet}>
+            Qoshish
+          </button>
+        </div>
+        <div className="flex-1 rounded-2xl p-5 flex shadow-lg">
+          <input type="search" placeholder="Qidirish..." />
+        </div>
+      </main>
+      <section className="flex justify-between" >
+        <div className="flex gap-5">
+          <button className=" boder shadow-lg p-4 rounded-2xl active:opacity-75 active" value={"all"}  onClick={el => ischek(el.target)}>Barchasi</button>
+          <button className=" boder shadow-lg p-4 rounded-2xl active:opacity-75" value={"notChek"} onClick={el => ischek(el.target)}>Faol</button>
+          <button className=" boder shadow-lg p-4 rounded-2xl active:opacity-75" value={"cheked"} onClick={el => ischek(el.target)}> Bajarigan</button>
+        </div>
+        <p>
+          Qolgan: <span></span>
+        </p>
+      </section>
+      <div>
+        <ul className="rounded-2xl shadow-2xl p-5">
+          {
+           ( isCheked === "all" ? todo : isCheked === "notChek" ? todo.filter(item => !item.status) : todo.filter(item  => item.status)).map(el=>{
+            return <li className="flex justify-between items-center" key={el.id}>
+              <div className="flex items-center p-4 gap-5">
+                <div className="">
+                  <input
+                  type="checkbox" className=" bg-amber-600"
+                  checked={el.status} 
+                  onChange={() => toggleStatus(el.id)}
+                />
+                </div>
+                <div>
+                  <h1>{el.name}</h1>
+                  <p>{el.data}</p>
+                </div>
+              </div>
+              <div className="flex gap-5 items-center">
+                <input type="text" className="boder shadow-lg p-4 rounded-2xl active:opacity-75" placeholder="update"/>
+                <button className="boder shadow-lg p-4 rounded-2xl active:opacity-75">remove</button>
+              </div>
+            </li> 
+           })
+          }
+        </ul>
       </div>
     </div>
   );
 }
-
-export default App;
